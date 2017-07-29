57b7bc11f7449b48d7c4d20a6788025f0273d8dd
Satoshi Moriai <satoshi.moriai@gmail.com>
2017-07-30 01:43:14 +0900

    Display PHY mode.

--- wifistat.sh
+++ wifistat.sh
@@ -34,7 +34,7 @@ BEGIN {
 }
 /}/ {
 	if (dict) {
-		printf "%3d %3d %s %s %s\n", a["spairport_network_channel"], a["spairport_signal_noise"], a["spairport_network_bssid"], a["_name"], STAT
+		printf "%3d %3d %s %s %s %s\n", a["spairport_network_channel"], a["spairport_signal_noise"], a["spairport_network_bssid"], substr(a["spairport_network_phymode"],7), a["_name"], STAT
 		dict = 0
 	}
 }
