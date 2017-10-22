667273a3e02ac2fb601272883b5c971d49ef236b
Satoshi Moriai <satoshi.moriai@gmail.com>
2017-10-22 17:52:13 +0900

    Add .bash_profile.

---
+++ bash_profile.sh
@@ -0,0 +1,20 @@
+
+if [ -r $HOME/.profile ]; then
+	. $HOME/.profile
+fi
+
+function dirs() {
+	command dirs -v $@
+}
+
+function pushd() {
+	command pushd $@ >/dev/null ; dirs
+}
+
+function popd() {
+	command popd $@ >/dev/null; dirs
+}
+
+function openman() {
+	open x-man-page://$@
+}
